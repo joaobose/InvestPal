9730a32f9498d5a92f85695d9fc06613ebdfba0e
Joao Pinto <joaobose@gmail.com>
2019-12-18 05:51:56 -0400

    plots

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ import os
 import torch
 import models
 import numpy as np
+import matplotlib.pyplot as plt
 from parameters import *
 from dataset import *
 
@@ -35,57 +36,18 @@ train_accs = []
 validation_accs = []
 losses = []
 
-for epoch in range(epochs):
+try:
+    for epoch in range(epochs):
 
-    #------------------------------------- Train loop----------------------------------- #
-    dataset_done = False
-    minibatch_losses = []
-    minibatch_accs = []
-    
+        #------------------------------------- Train loop----------------------------------- #
+        dataset_done = False
+        minibatch_losses = []
+        minibatch_accs = []
+        
 
-    while True:
-        data, labels, dataset_done = dataset.get_batch(batch_size, 'train')
-
-        if dataset_done:
-            break
-        data = [torch.FloatTensor(timestep) for timestep in data]
-        labels = torch.FloatTensor(labels)
-
-        assert(len(data) == timesteps)
-
-        if str(device) == 'cuda':
-            data = [timestep.cuda() for timestep in data]
-            labels = labels.cuda()
-
-        model.zero_grad()
-        out = model(data)
-
-        loss = model.backpropagate(out,labels)
-
-        minibatch_losses.append(loss.item())
-
-        # Calculating accuracy
-        y_hat = out.cpu().detach().numpy()
-        y = labels.cpu().numpy()
-
-        acc = (y_hat > acc_threshold)
-        acc = (acc * 1 == y) * 1
-        acc = acc.sum() / len(y)
-        minibatch_accs.append(acc)
-
-    loss_mean = np.array(minibatch_losses).mean()
-    losses.append(loss_mean)
-
-    train_acc_mean = np.array(minibatch_accs).mean()
-    train_accs.append(train_acc_mean)
-    
-    #------------------------------------- Validation loop----------------------------------- #
-    dataset_done = False
-    minibatch_accs = []
-
-    with torch.no_grad():
         while True:
-            data, labels, dataset_done = dataset.get_batch(batch_size, 'validation')
+            data, labels, dataset_done = dataset.get_batch(batch_size, 'train')
+
             if dataset_done:
                 break
             data = [torch.FloatTensor(timestep) for timestep in data]
@@ -97,8 +59,12 @@ for epoch in range(epochs):
                 data = [timestep.cuda() for timestep in data]
                 labels = labels.cuda()
 
-            # model.zero_grad()
+            model.zero_grad()
             out = model(data)
+
+            loss = model.backpropagate(out,labels)
+
+            minibatch_losses.append(loss.item())
 
             # Calculating accuracy
             y_hat = out.cpu().detach().numpy()
@@ -109,11 +75,84 @@ for epoch in range(epochs):
             acc = acc.sum() / len(y)
             minibatch_accs.append(acc)
 
-        validation_acc_mean = np.array(minibatch_accs).mean()
-        validation_accs.append(validation_acc_mean)
+        loss_mean = np.array(minibatch_losses).mean()
+        train_acc_mean = np.array(minibatch_accs).mean()
 
-    #------------------------------------- Epoch output -------------------------------------- #
-    print('\nEpoch: {}'.format(epoch))
-    print('Loss: {}'.format(loss_mean))
-    print('Train Accuracy: {}'.format(train_acc_mean))
-    print('Validation Acurracy: {}'.format(validation_acc_mean))
+        if epoch % plot_save_freq:
+            losses.append(loss_mean) 
+            train_accs.append(train_acc_mean)
+        
+        #------------------------------------- Validation loop----------------------------------- #
+        dataset_done = False
+        minibatch_accs = []
+
+        with torch.no_grad():
+            while True:
+                data, labels, dataset_done = dataset.get_batch(batch_size, 'validation')
+                if dataset_done:
+                    break
+                data = [torch.FloatTensor(timestep) for timestep in data]
+                labels = torch.FloatTensor(labels)
+
+                assert(len(data) == timesteps)
+
+                if str(device) == 'cuda':
+                    data = [timestep.cuda() for timestep in data]
+                    labels = labels.cuda()
+
+                # model.zero_grad()
+                out = model(data)
+
+                # Calculating accuracy
+                y_hat = out.cpu().detach().numpy()
+                y = labels.cpu().numpy()
+
+                acc = (y_hat > acc_threshold)
+                acc = (acc * 1 == y) * 1
+                acc = acc.sum() / len(y)
+                minibatch_accs.append(acc)
+
+
+            validation_acc_mean = np.array(minibatch_accs).mean()
+
+            if epoch % plot_save_freq:
+                validation_accs.append(validation_acc_mean)
+
+        #------------------------------------- Epoch output -------------------------------------- #
+        print('\nEpoch: {}'.format(epoch))
+        print('Loss: {}'.format(loss_mean))
+        print('Train Accuracy: {}'.format(train_acc_mean))
+        print('Validation Acurracy: {}'.format(validation_acc_mean))
+
+except KeyboardInterrupt:
+    print('se acabo')
+
+    #------------------------------------- Loss plot -------------------------------------- #
+    plt.plot(losses, label="agent reward")
+    legend = plt.legend(loc='upper center', shadow=True)
+    plt.title('Loss plot')
+    plt.ylabel('Loss')
+    plt.xlabel('Epochs x' + str(plot_save_freq))
+    frame = legend.get_frame()
+    frame.set_facecolor('0.90')
+    plt.show()
+
+    #------------------------------------- Train acc plot -------------------------------------- #
+    plt.plot(train_accs, label="agent reward")
+    legend = plt.legend(loc='upper center', shadow=True)
+    plt.title('Train acc plot')
+    plt.ylabel('Train accuracy')
+    plt.xlabel('Epochs')
+    frame = legend.get_frame()
+    frame.set_facecolor('0.90')
+    plt.show()
+
+    #------------------------------------- Validation acc plot -------------------------------------- #
+    plt.plot(validation_accs, label="agent reward")
+    legend = plt.legend(loc='upper center', shadow=True)
+    plt.title('Validation acc plot')
+    plt.ylabel('Validation accuracy')
+    plt.xlabel('Epochs')
+    frame = legend.get_frame()
+    frame.set_facecolor('0.90')
+    plt.show()
